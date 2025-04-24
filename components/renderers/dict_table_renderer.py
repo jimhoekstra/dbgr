44833992b2a136b9322c86802e5561444188f4ad
Jim Hoekstra <jimhoekstra2@gmail.com>
2025-04-24 22:34:57 +0200

    Revert "simplify dbgr and add textual app for viewing dataframes"
    This reverts commit cd8f09372d4476e7a73c4f8f7efa2012d9960269.

--- components/renderers/dict_table_renderer.py
+++ components/renderers/dict_table_renderer.py
@@ -4,8 +4,6 @@ from rich.padding import Padding
 from rich.console import Console, RenderResult, ConsoleOptions, Group
 from rich.text import Text
 
-import pandas as pd
-
 
 class DictTableRenderer:
     def __init__(
@@ -25,24 +23,18 @@ class DictTableRenderer:
             highlight=True,
             box=SIMPLE,
             row_styles=["on grey11", ""],
-            width=width - 10,
+            width=width,
             style="white",
         )
 
-        table.add_column("[bright_black]Key", style="cyan", justify="right")
-        table.add_column("[bright_black]Value", justify="left")
-        table.add_column("[bright_black]Type", style="magenta italic")
+        table.add_column("[grey50]key", style="cyan", justify="right")
+        table.add_column("[grey50]value", justify="left")
+        table.add_column("[grey50]type", style="magenta italic")
 
         for k, v in self.dict_to_render.items():
-            value = repr(v)
-            if isinstance(v, pd.DataFrame):
-                value = f"[bold]DataFrame[{len(v)} rows, {len(v.columns)} columns]"
-            if len(value) > 50:
-                value = value[:30] + "..."
-
             table.add_row(
                 Padding(k, (0, 0)),
-                Padding(value, (0, 0)),
+                Padding(repr(v), (0, 0)),
                 Padding(v.__class__.__name__, (0, 0)),
             )
 
--- dbgr.py
+++ dbgr.py
@@ -1,4 +1,4 @@
-from sys import exit
+from sys import exit, settrace, gettrace
 from pathlib import Path
 from types import FrameType
 from enum import StrEnum
@@ -14,7 +14,6 @@ from rich.layout import Layout
 
 from components.renderers.source_code_renderer import SourceCodeRenderer
 from components.renderers.dict_table_renderer import DictTableRenderer
-from dataframes.viewer import DataFrameViewer
 
 
 class DebuggerMode(StrEnum):
@@ -25,11 +24,6 @@ class DebuggerMode(StrEnum):
     EXIT = "exit"
 
 
-debugger_state = {
-    "mode": DebuggerMode.CONTINUE,
-}
-
-
 @dataclass
 class FrameInfo:
     """Dataclass to hold frame information."""
@@ -38,15 +32,21 @@ class FrameInfo:
     line_number: int
 
 
-def is_user_frame(frame: FrameType, user_dir: Path) -> bool:
+# Global debugger state
+debugger_state = {
+    "mode": "continue",  # can be "step" or "continue"
+    "user_dir": None,
+    "full_screen": False,
+}
+
+
+def is_user_frame(frame: FrameType) -> bool:
     """Check if the frame is part of the user's code.
 
     Parameters
     ----------
     frame
         The frame to check.
-    user_dir
-        The directory of the user's code (i.e. the code that is being debugged).
 
     Returns
     -------
@@ -59,110 +59,139 @@ def is_user_frame(frame: FrameType, user_dir: Path) -> bool:
     if ".venv" in str(filename):
         return False
 
-    if "<" in str(filename) and ">" in str(filename):
-        return False
-
+    assert debugger_state["user_dir"] is not None, "User directory not set"
     # Check that the file of the frame is in the user's directory
-    if not filename.is_relative_to(user_dir):
+    if not filename.is_relative_to(debugger_state["user_dir"]):
         return False
 
     return True
 
 
-def _get_frame_info(frame: FrameType) -> FrameInfo:
-    """Get frame information."""
-    filename = Path(frame.f_code.co_filename).resolve()
-    line_number = frame.f_lineno
-    return FrameInfo(filename, line_number)
+class Debugger:
+    user_dir: Path
+    mode: DebuggerMode
+    full_screen: bool
+    n_source_lines: int
+
+    def __init__(self, user_dir: Path) -> None:
+        self.user_dir = user_dir
+        self.mode = DebuggerMode.CONTINUE
+        self.full_screen = False
+        self.n_source_lines = 20
+
+    def local_trace(self, frame: FrameType, event, arg):
+        if not is_user_frame(frame):
+            return self.local_trace  # Skip stepping into debugger code
+
+        if event == "line":
+            self.dbgr_trace(frame)
+        elif event == "call":
+            print(f"dbgr: Function call {frame.f_code.co_name}")
+        else:
+            print(f"dbgr: Event {event} in {frame.f_code.co_name}")
+        return self.local_trace
+
+    def global_trace(self, frame, event, arg):
+        if not is_user_frame(frame):
+            return self.global_trace  # Skip stepping into debugger code
+
+        if event == "call":
+            self.dbgr_trace(frame)
+            return self.local_trace
+        return None
+
+    @staticmethod
+    def _get_frame_info(frame: FrameType) -> FrameInfo:
+        """Get frame information."""
+        filename = Path(frame.f_code.co_filename).resolve()
+        line_number = frame.f_lineno
+        return FrameInfo(filename, line_number)
+
+    def dbgr_trace(self, frame: FrameType) -> None:
+        trace_fn = gettrace()
+        settrace(None)  # Temporarily disable to avoid recursion in dbgr
+
+        frame_info = self._get_frame_info(frame)
+
+        prev_line = (
+            frame_info.path_to_file.read_text().splitlines()[frame_info.line_number - 2]
+            if frame_info.line_number > 1
+            else ""
+        )
+
+        if (
+            prev_line.strip().startswith("# breakpoint")
+            or self.mode == DebuggerMode.STEP
+        ):
+            self.debug_frame(frame)
+
+        settrace(trace_fn)  # Re-enable the original trace function
+
+    def debug_frame(self, frame: FrameType) -> None:
+        frame_info = self._get_frame_info(frame)
+
+        _, lines = get_terminal_size()
+        height = lines - 1 if debugger_state["full_screen"] else self.n_source_lines + 2
+
+        console = Console(height=height)
+        layout = Layout(name="root")
+        layout.split_row(
+            Layout(name="left"),
+            Layout(name="right"),
+        )
+
+        syntax = SourceCodeRenderer(
+            frame_info.path_to_file,
+            frame_info.line_number,
+            max_lines=self.n_source_lines,
+            border_color="blue",
+        )
+
+        locals_table = DictTableRenderer(
+            frame.f_locals,
+            title="[bold]Local Variables",
+            height=self.n_source_lines + 2,
+            border_color="blue",
+        )
+
+        console.print(
+            f"[yellow]{datetime.now()} dbgr: Encountered breakpoint",
+        )
+
+        layout["left"].update(syntax)
+        layout["right"].update(locals_table)
+        console.print(layout)
+
+        command = Prompt.ask(
+            "[bold][yellow] >>>",
+            choices=["c", "s", "e"],
+            default="s" if debugger_state["mode"] == "step" else "c",
+        )
+
+        if command == "e":
+            console.print(f"[yellow]{datetime.now()} dbgr: Exiting debugger")
+            self.mode = DebuggerMode.EXIT
+            raise exit(0)
+        elif command == "s":
+            console.print(f"[yellow]{datetime.now()} dbgr: Stepping into the next line")
+            debugger_state["mode"] = "step"
+            self.mode = DebuggerMode.STEP
+        elif command == "c":
+            console.print(f"[yellow]{datetime.now()} dbgr: Continuing execution")
+            debugger_state["mode"] = "continue"
+            self.mode = DebuggerMode.CONTINUE
+        else:
+            console.print(f"[yellow]{datetime.now()} dbgr: Invalid command")
+            raise exit(0)
 
 
-def debug_frame(
-    # self,
-    frame: FrameType,
-    title: str,
-    n_source_lines: int,
-    panel_color: str = "blue",
-    prompt: bool = True,
-) -> None:
-    frame_info = _get_frame_info(frame)
-
-    _, lines = get_terminal_size()
-    height = lines - 1 if False else n_source_lines + 2
-
-    console = Console(height=height)
-    layout = Layout(name="root")
-    layout.split_row(
-        Layout(name="left"),
-        Layout(name="right"),
-    )
-
-    syntax = SourceCodeRenderer(
-        frame_info.path_to_file,
-        frame_info.line_number,
-        title,
-        max_lines=n_source_lines,
-        border_color=panel_color,
-    )
-
-    locals_table = DictTableRenderer(
-        {k: v for k, v in frame.f_locals.items() if not k.startswith("__")},
-        title="[bold]Local Variables",
-        height=n_source_lines + 2,
-        border_color=panel_color,
-    )
-
-    console.print(
-        f"[yellow]{datetime.now()} dbgr: Encountered breakpoint",
-    )
-
-    layout["left"].update(syntax)
-    layout["right"].update(locals_table)
-    console.print(layout)
-
-    if prompt:
-        exit_command = False
-        while not exit_command:
-            console.print(
-                "[red italic]Enter command (e: exit, s: step, c: continue, d <df_name>: display DataFrame):"
-            )
-            command = Prompt.ask(
-                "[bold][yellow] >>>",
-            )
-
-            if command == "e":
-                console.print(f"[yellow]{datetime.now()} dbgr: Exiting debugger")
-                debugger_state["mode"] = DebuggerMode.EXIT
-                exit_command = True
-                raise exit(0)
-            elif command == "s":
-                console.print(
-                    f"[yellow]{datetime.now()} dbgr: Stepping into the next line"
-                )
-                debugger_state["mode"] = DebuggerMode.STEP
-                exit_command = True
-            elif command == "c":
-                console.print(f"[yellow]{datetime.now()} dbgr: Continuing execution")
-                debugger_state["mode"] = DebuggerMode.CONTINUE
-                exit_command = True
-            elif command.startswith("d"):
-                df_name_to_display = command.split(" ")[1]
-                df_to_display = frame.f_locals.get(df_name_to_display)
-                if df_to_display is None:
-                    console.print(
-                        f"[yellow]{datetime.now()} dbgr: DataFrame '{df_name_to_display}' not found in local variables"
-                    )
-                    continue
-                app = DataFrameViewer(df_to_display)
-                app.run()
-            elif command == "":
-                pass
-            else:
-                console.print(f"[yellow]{datetime.now()} dbgr: Invalid command")
-
-
-def dbgr():
-    """Run the debugger."""
+def run_dbgr():
     stack = inspect.stack()
     caller_frame = stack[1]
+    caller_file = caller_frame.filename
+    debugger_state["user_dir"] = Path(caller_file).parent
 
-    debug_frame(caller_frame.frame, "Current Frame", 15)
+    dbgr = Debugger(debugger_state["user_dir"])
+
+    # Set the global trace function to start debugging
+    settrace(dbgr.global_trace)
